9d0ebb7f39c62aa0205166a8b35f944f37f456bb
Fernando Rodriguez <fernando.ale.fars@gmail.com>
2024-08-26 18:38:49 -0400

    Bisiesto entre 4

--- src/Bisiesto.js
+++ src/Bisiesto.js
@@ -1,6 +1,11 @@
 function Bisiestoentre400(numero) {
   if(numero%400==0)
    return numero,"Es bisiesto";
+};
+function NoBisesto100(numero){
+    if(numero%100==0 && numero%400!=0)
+      return numero,"No es bisiesto";
 }
 
-export default Bisiestoentre400;
+
+export {Bisiestoentre400,NoBisesto100};
--- src/Bisiesto.test.js
+++ src/Bisiesto.test.js
@@ -1,7 +1,16 @@
-import Bisiestoentre400 from "./Bisiesto.js";
+import {Bisiestoentre400,NoBisesto100} from "./Bisiesto.js";
 
 describe("Ver si es divisible entre 400", () => {
   it("Deberia identificar si es divisible entre 400", () => {
     expect(Bisiestoentre400(1600)).toEqual("Es bisiesto");
   });
+  it("Deberia Mostrar cuando un anio no es bisiesto",()=>{
+    expect(NoBisesto100(1000)).toEqual("No es bisiesto");
+  });
+  it("Deberia saber si un cualquier anio es bisiesto o no",()=>{
+    expect(BisiestoEntre4(2012)).toEqual("Es Bisiesto");
+  })
+ 
 });
+
+  
